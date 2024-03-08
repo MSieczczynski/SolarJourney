1bf92a9c5a48da629878b2204a2d3624053036cc
Michał Sieczczyński <msieczczynski@gmail.com>
2024-03-08 15:09:30 +0100

    Third commit

--- src/main/java/game/solarjourney/MenuApplication.java
+++ src/main/java/game/solarjourney/MenuApplication.java
@@ -11,7 +11,7 @@ public class MenuApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(MenuApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MenuApplication.class.getResource("MenuView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello World!");
         stage.setScene(scene);
