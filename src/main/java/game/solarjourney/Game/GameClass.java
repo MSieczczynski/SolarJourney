78d97a48e855984dae8ff510f0aefcec450409b0
Michał Sieczczyński <msieczczynski@gmail.com>
2024-05-10 18:10:12 +0200

    Ruch planet

--- src/main/java/game/solarjourney/Game/GameClass.java
+++ src/main/java/game/solarjourney/Game/GameClass.java
@@ -41,17 +41,39 @@ class Odmalowanie implements Runnable{
         });
     }
 }
+
+class PlanetControl implements Runnable
+{
+    private GameController controller;
+    public PlanetControl(GameController controller)
+    {
+        this.controller = controller;
+    }
+
+    @Override
+    public void run()
+    {
+        Platform.runLater(() -> {
+            controller.planetPosition();
+        });
+    }
+}
+
 public class GameClass extends Application{
     public static double start;
     public static double sceneWidth;
     public double sceneHeight;
     private ScheduledExecutorService executor;
+    private ScheduledExecutorService executor1;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("GameView.fxml"));
         Parent root = loader.load();
         GameController controller = loader.getController();
         Scene scene = new Scene(root);
+
+        executor1 = Executors.newScheduledThreadPool(1);
+        executor1.scheduleAtFixedRate(new PlanetControl(controller), 0, 1, TimeUnit.MILLISECONDS);
 
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
@@ -81,6 +103,7 @@ public class GameClass extends Application{
                         System.out.println("Klawisz Escape został naciśnięty");
                         if (executor != null && !executor.isShutdown()) {
                             executor.shutdown();
+                            executor1.shutdown();
                         }
                         break;
                     default:
--- src/main/java/game/solarjourney/Menu/MenuController.java
+++ src/main/java/game/solarjourney/Menu/MenuController.java
@@ -1,5 +1,6 @@
 package game.solarjourney.Menu;
 
+import game.solarjourney.Settings.SettingsController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
