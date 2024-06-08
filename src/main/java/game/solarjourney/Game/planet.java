eb84f9913928902006ed5e5fb9a3dcb6a6ee07e7
zaba04 <kodymanna770@gmail.com>
2024-06-08 20:51:25 +0200

    kolorki na planetach

--- src/main/java/game/solarjourney/Game/planet.java
+++ src/main/java/game/solarjourney/Game/planet.java
@@ -1,19 +1,8 @@
 package game.solarjourney.Game;
-
-import javafx.scene.shape.Circle;
-
 //autor: Anna Kodym v
 public class planet {
-    public int planetid = 0; //do ustawiania planety końcowej
-    public Circle planet;
     public int radius;
     public double x,y;
-    public void setPlanet(Circle planet){
-        this.planet = planet;
-    }
-    public void setPlanetid(){
-        planetid = 1;
-    }
     public double getX(){
         return this.x;
     }
--- src/main/java/game/solarjourney/Game/rocket.java
+++ src/main/java/game/solarjourney/Game/rocket.java
@@ -1,19 +1,15 @@
 package game.solarjourney.Game;
 
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 
 //Autor: Anna Kodym
 public class rocket {
     public int radius = 10;
     public double x,y;
-    //public Circle rocket;
-    public Rectangle rocket;
-    public void setRocket(Rectangle rocket){
-        this.rocket = rocket;
-    }
+
     public boolean collidesWith(planet planet) {
         double distance = Math.sqrt(Math.pow(x - planet.getX(), 2) + Math.pow(y - planet.getY(), 2));
         return distance <= radius + planet.getRadius();
     }
 }
+//control check d
